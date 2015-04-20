3a02f552aec9b240cc5020b0b9a5bab35eb59ab9
Lyman Lai <lyman.lai@yaha.me>
2015-04-20 10:46:40 +0800

    change some default setting in config.default.js file

--- config.default.js
+++ config.default.js
@@ -39,8 +39,8 @@ var config = {
     cnzz_tracker_id: '',
 
     // mongodb 配置
-    db: 'mongodb://127.0.0.1/node_club_dev',
-    db_name: 'node_club_dev',
+    db: 'mongodb://127.0.0.1/wildfire',
+    db_name: 'wildfire',
 
     // redis 配置，默认是本地
     redis_host: '127.0.0.1',

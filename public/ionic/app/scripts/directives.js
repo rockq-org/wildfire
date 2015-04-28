dcd1678a101b920f6dc6ae1750ca284d2a6d130c
Lyman Lai <lyman.lai@yaha.me>
2015-04-28 19:00:51 +0800

    #84 make reset button done

--- public/ionic/app/scripts/directives.js
+++ public/ionic/app/scripts/directives.js
@@ -72,11 +72,18 @@ angular.module('iwildfire.directives', [])
 .directive('chooseLocation', function($timeout, $document) {
     var init = function(element, attrs, scope, width, height) {
         //初始化地图
+        var center = new qq.maps.LatLng(scope.locationDetail.latitude, scope.locationDetail.longitude);
+        var newCenter = new qq.maps.LatLng(scope.locationDetail.latitude, scope.locationDetail.longitude);
         var map = new qq.maps.Map(element, {
             // 地图的中心地理坐标
-            center: new qq.maps.LatLng(scope.locationDetail.latitude, scope.locationDetail.longitude),
+            center: center,
             zoom: 17
         });
+        var address = scope.locationDetail.api_address;
+        var info = new qq.maps.InfoWindow({
+            map: map
+        });
+
         //创建自定义控件
         var middleControl = document.createElement("div");
         middleControl.style.left = (width - 36) / 2 + "px";
@@ -87,6 +94,26 @@ angular.module('iwildfire.directives', [])
         middleControl.style.zIndex = "100000";
         middleControl.innerHTML = '<img src="https://www.cdlhome.com.sg/mobile_assets/images/icon-location.png" />';
         element.appendChild(middleControl);
+        // qq.maps.event.addListener(middleControl, 'click', function(){
+        //     info.open();
+        //     info.setContent('<div style="text-align:center;white-space:nowrap;'+
+        //     'margin:10px;">' + address + '</div>');
+        //     info.setPosition(center);
+        // });
+
+        var resetControl = document.createElement("div");
+        resetControl.style.left = 15 + "px";
+        resetControl.style.top = height - 200 + "px";
+        resetControl.style.position = "relative";
+        resetControl.style.width = "36px";
+        resetControl.style.height = "36px";
+        resetControl.style.zIndex = "100000";
+        resetControl.innerHTML = '<img src="https://www.cdlhome.com.sg/mobile_assets/images/icon-location.png" />';
+        element.appendChild(resetControl);
+        qq.maps.event.addListener(resetControl, 'click', function() {
+            console.log('here!');
+            map.panTo( center );
+        });
 
         //当地图中心属性更改时触发事件
         qq.maps.event.addListener(map, 'center_changed', center_changed);
@@ -99,11 +126,12 @@ angular.module('iwildfire.directives', [])
             complete: function(result) {
                 $timeout(function() {
                     var c = result.detail.addressComponents;
-                    var address = c.province + c.city + c.district + c.street + c.streetNumber + c.town + c.village;
+                    address = c.city + c.district + c.street + c.streetNumber + c.town + c.village;
                     scope.$parent.$parent.locationDetail.api_address = result.detail.address;
                     scope.$parent.$parent.locationDetail.user_edit_address = address;
                     scope.$parent.$parent.locationDetail.latitude = result.detail.location.lat;
                     scope.$parent.$parent.locationDetail.longitude = result.detail.location.lng;
+                    newCenter = new qq.maps.LatLng(result.detail.location.lat, result.detail.location.lng);
                 });
             }
         });

6364e1f3a2e8005c643a2665de77912ed148b29b
Samurais <hailiang.hl.wang@gmail.com>
2015-05-13 06:45:23 +0800

    #267 change folder structure

---
+++ client/app/scripts/configuration.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ angular.module('config', [])
+
+.constant('cfg', {mode:'development',server:'http://localhost:3000',api:'http://localhost:3000/api/v1',debug:true,node:{mini_assets:false,qn_access:{accessKey:'MuDBhibABT6T85B1mA5bqoolYVCmGUizJ2CMS_Wn',secretKey:'x3W_dZVS75w6HraQciomlDfn_NC2hqPl4qfB1rF9',bucket:'wildfirepro',domain:'7xiyud.com1.z0.glb.clouddn.com',host:'http://7xiyud.com1.z0.glb.clouddn.com'}}})
+
+;

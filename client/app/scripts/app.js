694bd5a997e0681504f33c65e7a8a2e5caf23f15
nihgwu <nihgwu@live.com>
2015-05-16 17:19:24 +0800

    #281 removed cordova related codes

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -51,19 +51,6 @@ angular.module('iwildfire', ['ionic',
         $ionicLoading.hide();
     };
 
-    $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            // org.apache.cordova.statusbar required
-            StatusBar.styleLightContent();
-        }
-
-    });
-
     // error handler
     var errorMsg = {
         0: '网络出错啦，请再试一下',
